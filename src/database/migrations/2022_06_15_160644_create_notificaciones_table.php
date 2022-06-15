35054b8e617fe40919e8d97f87f13ae1469ffd90
admin <malavevillamizar.d@gmail.com>
2022-06-15 12:28:49 -0400

    tareas/notificaciones/plantillas bases

---
+++ src/database/migrations/2022_06_15_160644_create_notificaciones_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificacionesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notificaciones', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('plantilla_id');
+            $table->string('telefono')->nullable();
+            $table->string('email')->nullable();
+            $table->boolean('notificar_email');
+            $table->boolean('notificar_sms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notificaciones');
+    }
+}
---
+++ src/database/migrations/2022_06_15_161115_create_plantillas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlantillasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plantillas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->text('descripcion_larga');
+            $table->string('descripcion_corta');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plantillas');
+    }
+}
