fed72d1fcc557b1219b7365ed49b1033303ba04a
admin <malavevillamizar.d@gmail.com>
2022-06-23 20:07:32 -0400

    the cron route shouldn't require authentication

--- src/app/Http/Controllers/TareaController.php
+++ src/app/Http/Controllers/TareaController.php
@@ -26,7 +26,7 @@ class TareaController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except('cron');;
     }
     
     public function index()
